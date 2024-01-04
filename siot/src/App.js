b97f4a34d1855a1ff3423d3071dabf6e17136b9e
quantbrainz <quantbrainz@gmail.com>
2024-01-04 16:14:25 +0900

    1/4 16

--- siot/src/App.js
+++ siot/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> SIOT Commute System {/* 수정 필요 */}
+        SIOT Commute System 제발 되라  ㄴㅇㅅ !!!!!!!{/* 수정 필요 */}
 
         </p>
         <a
